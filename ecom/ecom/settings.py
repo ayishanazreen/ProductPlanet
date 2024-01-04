e92d355283b0ce3ab71b29134dfc3e48479e19e6
Ayisha Nazreen <143947362+ayishanazreen@users.noreply.github.com>
2024-01-04 10:28:19 +0400

    settings.py

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)t!xmypb&06@x(xq8m2k+j!@j+$pc+g^52#dzvn#pxibw&t-mc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
